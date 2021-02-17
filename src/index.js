f24c3f4deeff882d0baa099799d012875b35afa0
Lansyfonseka <twinkstriker@gmail.com>
2021-02-17 17:10:14 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let rules = new RegExp(bracketsConfig.map(e => {
+        console.log(typeof(e[0] - 1));
+        if (e[0].search(/1|2|3|4|5|6|7|8|9/) != -1) return (e[0] + e[1])
+        else
+            return '\\' + e[0] + '\\' + e[1];
+    }).join('|'));
+    for (let i = str.length; i > 0; i--) {
+        str = str.replace(rules, '');
+    }
+    return str == '' ? true : false;
 }
